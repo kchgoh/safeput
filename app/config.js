3f6a0e757ac3791d32934f0ca13591e88dc3b085
Ken Goh <ken.c.goh@gmail.com>
2016-11-26 11:28:40 +0900

    Allow specify env-specific config in env vars

--- app/config.js
+++ app/config.js
@@ -1,16 +1,16 @@
 module.exports = {
-	jwtSecret: "default",
-	sitePass: "hi",
+	jwtSecret: process.env.JWT_SECRET || "default",
+	sitePass: process.env.SITE_PASS || "hi",
 	accessTokenExpire: 60,	// in sec
 	refreshTokenExpire: 300,
 	downloadTokenExpire: 10,
-	port: "8081",
+	port: process.env.PORT || "8081",
 	viewsDir: "views",
 	resourcesDir: "public",
 	filesDir: "uploads",
 	thumbnailsDir: "thumbnails",
 	tmpDir: "tmp",
 	dbFilePath: "database/store.db",
-	sslKeyPath: "../../keys/ssl.key",
-	sslCertPath: "../../keys/ssl.crt"
+	sslKeyPath: process.env.SSL_KEY_PATH || "../../keys/ssl.key",
+	sslCertPath: process.env.SSL_CERT_PATH || "../../keys/ssl.crt"
 };
